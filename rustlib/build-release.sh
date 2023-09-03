ce15146c909888e5f10548b1de6d8d54e05a66fa
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-09-04 02:13:26 +0300

    feat: ✨ build debug and release scripts added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

---
+++ rustlib/build-release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cargo update
+
+rustup target add \
+    aarch64-linux-android \
+    armv7-linux-androideabi \
+    x86_64-linux-android \
+    i686-linux-android
+
+cargo install cargo-ndk
+for i in armeabi-v7a arm64-v8a x86 x86_64;do cargo ndk -t ${i} -o ../app/src/main/jniLibs/  build --release;done
