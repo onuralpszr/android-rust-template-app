929faf524c72c1b36e28e7c1f860f458eebe1464
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-09-03 22:20:05 +0300

    feat: ✨ ignore snake case because of jin fn and unused modules removed
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -1,34 +1,7 @@
-// This is the interface to the JVM that we'll
-// call the majority of our methods on.
 use jni::JNIEnv;
+use jni::objects::{JClass, JString};
 
-// These objects are what you should use as arguments to your native function.
-// They carry extra lifetime information to prevent them escaping from the
-// current local frame (which is the scope within which local (temporary)
-// references to Java objects remain valid)
-use jni::objects::{GlobalRef, JClass, JObject, JString};
-
-use jni::objects::JByteArray;
-use jni::sys::{jint, jlong};
-
-use std::{sync::mpsc, thread, time::Duration};
-
-// This `#[no_mangle]` keeps rust from "mangling" the name and making it unique
-// for this crate. The name follow a strict naming convention so that the
-// JNI implementation will be able to automatically find the implementation
-// of a native method based on its name.
-//
-// The `'local` lifetime here represents the local frame within which any local
-// (temporary) references to Java objects will remain valid.
-//
-// It's usually not necessary to explicitly name the `'local` input lifetimes but
-// in this case we want to return a reference and show the compiler what
-// local frame lifetime it is associated with.
-//
-// Alternatively we could instead return the `jni::sys::jstring` type instead
-// which would represent the same thing as a raw pointer, without any lifetime,
-// and at the end use `.into_raw()` to convert a local reference with a lifetime
-// into a raw pointer.
+#[allow(non_snake_case)]
 #[no_mangle]
 pub extern "system" fn Java_com_os_androidrusttemplate_MainActivityKt_hello<'local>(
     // Notice that this `env` argument is mutable. Any `JNIEnv` API that may
