04ac068efa19dddc93723d6132d8df35dbca55f3
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-09-03 21:30:10 +0300

    fix: 🐞 rustlib compile error fixed
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- rustlib/src/lib.rs
+++ rustlib/src/lib.rs
@@ -3,7 +3,7 @@ use log::info;
 
 #[no_mangle]
 fn android_main(app: AndroidApp) {
-    android_logger::init_once(android_logger::Config::default().level(log::Level::Info));
+    android_logger::init_once(android_logger::Config::default().with_max_level(log::LevelFilter::Info));
 
     let mut quit = false;
     let mut redraw_pending = true;
